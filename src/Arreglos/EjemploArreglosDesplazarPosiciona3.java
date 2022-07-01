9e95d860e21620692e796da7237bed70de45ca8e
davidqrss31 <74071355+davidqrss31@users.noreply.github.com>
2022-07-01 12:49:44 +0200

    010722

--- src/Arreglos/EjemploArreglosDesplazarPosiciona3.java
+++ src/Arreglos/EjemploArreglosDesplazarPosiciona3.java
@@ -7,31 +7,38 @@ public class EjemploArreglosDesplazarPosiciona3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] a = new int[10];
-		int elemento, posicion;
+		int[] a = new int[7];
+		int numero, posicion;
 		Scanner sc = new Scanner(System.in);
 		
-		
-		for (int i = 0; i < a.length-1; i++) {
-			System.out.print("Ingrese el n�mero: ");
-			a[i] = sc.nextInt(); 
+		for(int i = 0; i < a.length - 1; i++) {
+			System.out.println("Inserte un núumero: ");
+			a[i] = sc.nextInt();
+			
 		}
 		System.out.println();
 		
-		System.out.println("Nuevo elemento:");
-		elemento = sc.nextInt();
+		System.out.println("Ingrese un número a ingresar: ");
+		numero = sc.nextInt();
 		
-		System.out.println("Posicion donde agregar del 0 al 9");
-		posicion = sc.nextInt();
+		posicion = 0;
 		
-		for (int i = a.length -2; i >= posicion; i--) {
+		while (posicion < 6 && numero > a[posicion]) {
+			posicion++;
+			
+		}
+		
+		for(int i = a.length -2; i>= posicion; i--) {
 			a[i+1] = a[i];
+			
 		}
-		a[posicion] = elemento;
+		a[posicion] = numero;
+		System.out.println("El número queda ordenador en: ");
+		
 		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+			System.out.println(i + ":" + a[i]);
 		}
-
+	
 	}
 
 }
