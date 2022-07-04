14a5bbec34a98e7fcc8bdf7ea474e97cc3627618
davidqrss31 <74071355+davidqrss31@users.noreply.github.com>
2022-07-04 21:14:33 +0200

    2

--- src/ArreglosEjercicios/ArraysconOrden.java
+++ src/ArreglosEjercicios/ArraysconOrden.java
@@ -1,7 +1,10 @@
 package ArreglosEjercicios;
 
 public class ArraysconOrden {
-
+/*
+ * Leer 10 números enteros desde el teclado para llenar un arreglo de 10 elementos. Luego debemos mostrarlos en el siguiente orden: 
+ * el último, el primero, el penúltimo, el segundo, el antepenúltimo, el tercero, y así sucesivamente.
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
